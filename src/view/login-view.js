b8e9e2b27d0d275b80ba53a3badaab2f59cf2e99
omairapalacios <cpalaciosanchez@hotmail.com>
2019-12-17 14:13:52 -0500

    update styles to login and register

--- src/view/login-view.js
+++ src/view/login-view.js
@@ -1,14 +1,18 @@
 export default () => {
   const loginView = `
   <form class="form-login" action="">
-    <p>Bienvenidx</p>
-    <input type="email" id="email-login" placeholder="Correo Electronico" required>
-    <input type="password" id="password-login" placeholder="Contraseña" required>
+    <h3>Bienvenidx</h3>
+    <div class="input-container">
+      <input type="email" id="email-login" placeholder="Correo Electronico" required>
+      <input type="password" id="password-login" placeholder="Contraseña" required>
+    </div>
     <button id="btn-login" class="btn-general" type="submit">INGRESAR</button>
-    <p>ó ingresa con :</p>
-    <button id="btn-facebook" class="btn-social-icon" type="submit" ><img src="../img/facebook.png" alt="facebook"></button>
-    <button id="btn-google" class="btn-social-icon" type="submit"><img src="../img/google.png" alt="gmail"></button>
-    <p>¿No tienes una cuenta?<a href="#/register">Regístrate</a></p>
+    <p>ó bien ingresa con...</p>
+    <div class="btn-social-net">
+      <button id="btn-facebook" class="btn-social-icon" type="submit" ><img src="../img/facebook.png" alt="facebook"></button>
+      <button id="btn-google" class="btn-social-icon" type="submit"><img src="../img/google.png" alt="gmail"></button>
+    </div>
+    <p>¿No tienes una cuenta? <a href="#/register">Regístrate</a></p>
     </form>`;
   const divELem = document.createElement('div');
   divELem.innerHTML = loginView;
--- src/view/register-view.js
+++ src/view/register-view.js
@@ -1,12 +1,15 @@
 
 export default () => {
   const registerView = `
-    <p>Bienvenidx</p>
+  <form class="form-register" action="">
+    <h3>Bienvenidx</h3>
+    <div class="input-container">
       <input id="username" type="text" placeholder="Nombre de usuario">
       <input id="email-register" type="email" placeholder="Correo Electronico">
       <input id="password-register" type="password" placeholder="Contraseña">
-      <button id="btn-register" class="btn-general" type="submit">REGISTRAME</button>
-    `;
+    </div>
+    <button id="btn-register" class="btn-general" type="submit">REGISTRARME</button>
+  </form>`;
   const divElem = document.createElement('div');
   divElem.className = 'viewRegister';
   divElem.innerHTML = registerView;
