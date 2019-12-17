00ce6cdd43fb795e75476db7dc5f3539d66906b8
omairapalacios <cpalaciosanchez@hotmail.com>
2019-12-16 19:02:28 -0500

    add styles to view login

--- src/view/login-view.js
+++ src/view/login-view.js
@@ -1,16 +1,15 @@
 export default () => {
   const loginView = `
-<p>Bienvenidx</p>
-  <form action="">
-    <p>Si ya tienes una cuenta</p>
+  <form class="form-login" action="">
+    <p>Bienvenidx</p>
     <input type="email" id="email-login" placeholder="Correo Electronico" required>
     <input type="password" id="password-login" placeholder="Contraseña" required>
     <button id="btn-login" class="btn-general" type="submit">INGRESAR</button>
-  </form>
-  <p>ó ingresa con :</p>
-  <button id="btn-facebook" class="btn-social-icon" type="submit" ><img src="../img/facebook.png" alt="facebook"></button>
-  <button id="btn-google" class="btn-social-icon" type="submit"><img src="../img/google.png" alt="gmail"></button>
-  <p>¿No tienes una cuenta?<a href="#/register">Regístrate</a></p>`;
+    <p>ó ingresa con :</p>
+    <button id="btn-facebook" class="btn-social-icon" type="submit" ><img src="../img/facebook.png" alt="facebook"></button>
+    <button id="btn-google" class="btn-social-icon" type="submit"><img src="../img/google.png" alt="gmail"></button>
+    <p>¿No tienes una cuenta?<a href="#/register">Regístrate</a></p>
+    </form>`;
   const divELem = document.createElement('div');
   divELem.innerHTML = loginView;
   return divELem;
