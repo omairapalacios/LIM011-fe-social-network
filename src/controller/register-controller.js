9eb89adc03aa1494c1716e7578e98c6892551bce
omairapalacios <cpalaciosanchez@hotmail.com>
2019-12-18 16:56:08 -0500

    add validatacions

--- src/controller/register-controller.js
+++ src/controller/register-controller.js
@@ -3,16 +3,12 @@ import { registerUserEmail } from '../model/auth-users.js';
 export default (event) => {
   event.preventDefault();
   const btnRegister = event.target;
-  const username = btnRegister.closest('form').querySelector('input[type=text]');
   const email = btnRegister.closest('form').querySelector('input[type=email]');
   const password = btnRegister.closest('form').querySelector('input[type=password]');
   const message = btnRegister.closest('form').querySelector('label');
 
-  registerUserEmail(email.value, password.value, username.value)
+  registerUserEmail(email.value, password.value)
     .then((result) => {
-      result.user.updateProfile({
-        displayName: username,
-      });
       const redirectLogin = {
         url: 'http://localhost:5000/',
       };
@@ -25,14 +21,15 @@ export default (event) => {
     .catch((error) => {
       const errorPassword = error.code;
       const errorEmail = error.message;
-      email.value = '';
-      password.value = '';
-      username.value = '';
+
       if (errorPassword === 'auth/weak-password') {
         message.innerHTML = 'La contraseña ingresada es débil, ingrese 6 o más caracteres';
+        password.value = '';
       } else if (errorEmail === 'auth/email-already-in-use') {
+        email.value = '';
         message.innerHTML = 'El correo ingresado ya se encuentra registrado';
       } else if (errorEmail === 'auth/invalid-email') {
+        email.value = '';
         message.innerHTML = 'El correo ingresado no es váĺido';
       }
     });
