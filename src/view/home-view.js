23a2c1d5043de0346fe363ac17a11b3d2162a0aa
MaricruzJ <maricruzj.er@gmail.com>
2019-12-18 17:42:26 -0500

    changes in mvc

--- src/view/home-view.js
+++ src/view/home-view.js
@@ -4,23 +4,25 @@ export default () => {
       <div class="front"></div>
       <div class="user-profile">
         <img src="" alt="" class="user-photo">
-        <span class="user-name">Fulana Suarez</span>
-        <p class="ocupation">Developer Jr.</p>
+        <div class="detail-user">
+          <span class="user-name">Fulana Suarez</span>
+          <p class="ocupation">Developer Jr.</p>
+        </div>
       </div>
     </section>
     <section class="container-posts">
       <div class="card-new-post">
-        <input type="text" placeholder="¿Qué quieres compartir?">
+        <textarea name="" id="" placeholder="¿Qué quieres compartir?"></textarea>
         <a href="http://" class="upload-picture">Subir<img src="" alt=""></a>
         <a href="http://" class="share-post"><button>Compartir</button></a>
       </div>
       <div class="card-post">
         <div class="header-post">
-          <span>Publicado por</span>
+          <span>Publicado por Fulanita Perez</span>
             <a href="http://">x</a>
         </div>
         <div class="detail-post">
-          <input type="text">
+         <textarea name="" id=""></textarea>
         </div>
         <div class="footer-post">
           <a href="http://" class="like-post">like<img src="" alt=""></a>
