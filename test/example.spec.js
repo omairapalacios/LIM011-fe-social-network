093455b70dd161579c1e6bcfa8bf2fe547f64885
omairapalacios <cpalaciosanchez@hotmail.com>
2019-12-19 18:31:02 -0500

    add test

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,7 +1,18 @@
 import { signInUserEmail } from '../src/model/auth-user.js';
 
-describe('test to  signInUserEmail function', () => {
-  it('debería ser una función', () => {
-    expect(typeof signInUserEmail).toBe('function');
-  });
+
+const firebasemock = require('firebase-mock');
+
+const mockAuth = new firebasemock.MockFirebase();
+mockAuth.autoFlush();
+global.firebase = firebasemock.MockFirebaseSdk(
+  () => null,
+  () => mockAuth,
+);
+
+describe('Sign In User', () => {
+  it('Debería poder iniciar sesión', () => signInUserEmail('omairapalacios@gmail.com', '123456')
+    .then((user) => {
+      expect(user.email).toBe('omairapalacios@gmail.com');
+    }));
 });
