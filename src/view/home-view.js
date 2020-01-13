148c6e974c0d52374a6d5bfff58151da2b676a4d
MaricruzJ <maricruzj.er@gmail.com>
2020-01-13 15:20:45 -0500

    update profile

--- src/view/home-view.js
+++ src/view/home-view.js
@@ -10,7 +10,7 @@ export default (dataPosts, dataUser) => {
         <img src='${dataUser.photoURL}' alt='' id='user-photo' class='user-photo'>
         <div class='detail-user'>
           <span id='user-name' class='name-user'>${dataUser.displayName}</span>
-          <span id='user-type'>${dataUser.typeUser}</span>
+          <span id='user-type'>Vendedor(a)</span>
         </div>
       </div>
     </section>
